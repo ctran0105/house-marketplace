2e9d0974c015a39fc6d002f11563634984a3574b
ctran0105 <catanh1520@gmail.com>
2023-09-04 15:32:14 -0400

    Initialize fetch single listing

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Explore from "./pages/Explore";
 import Offers from "./pages/Offers";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import Listing from "./pages/Listing";
 import ForgotPassword from "./pages/ForgotPassword";
 import CreateListing from "./pages/CreateListing";
 import Navbar from "./components/Navbar";
@@ -20,6 +21,10 @@ function App() {
           <Route path="/" element={<Explore />} />
           <Route path="/offers" element={<Offers />} />
           <Route path="/category/:categoryName" element={<Category />} />
+          <Route
+            path="/category/:categoryName/:listingId"
+            element={<Listing />}
+          />
           <Route path="/profile" element={<PrivateRoute />}>
             <Route path="/profile" element={<Profile />} />
           </Route>
