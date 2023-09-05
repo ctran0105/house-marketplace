7e922a8c554c011575616a51a8695bbdb120153d
ctran0105 <catanh1520@gmail.com>
2023-09-04 23:56:39 -0400

    Add Swiper to listing page. Use Swiper v6.8.1

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -9,6 +9,7 @@ import { getAuth } from "firebase/auth";
 import { db } from "../firebase.config";
 import Spinner from "../components/Spinner";
 import shareIcon from "../assets/svg/shareIcon.svg";
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
 
 function Listing() {
   const [listing, setListing] = useState(null);
